90e9f6a963a48db34a3cfec0bbd83480fddfab2a
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-24 16:50:34 +0100

    Added settings fragment and strings for page

--- app/src/main/java/uk/co/suskins/commutestatus/models/User.kt
+++ app/src/main/java/uk/co/suskins/commutestatus/models/User.kt
@@ -1,8 +1,6 @@
 package uk.co.suskins.commutestatus.models
 
 data class User(
-    val firstName: String,
-    val lastName: String,
     val email: String,
     val homeStation: String,
     val workStation: String
---
+++ app/src/main/java/uk/co/suskins/commutestatus/ui/commutestatus/settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package uk.co.suskins.commutestatus.ui.commutestatus.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
