d1dfa03ba26e4e304304beaa3460a36395280957
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-29 15:47:18 +0100

    Made Main Activity the welcome screen

---
+++ app/src/main/java/uk/co/suskins/commutestatus/ui/welcome/MainActivity.kt
@@ -0,0 +1,14 @@
+package uk.co.suskins.commutestatus.ui.welcome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import uk.co.suskins.commutestatus.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
