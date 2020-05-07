ca69d3157e86acefaa117559313cda6fb7356399
Ben Suskins <suskinsdevelopment@gmail.com>
2020-05-07 12:41:52 +0100

    Adde additional API endpoints to service for settings page

--- app/src/main/java/uk/co/suskins/commutestatus/service/CommuteStatusService.kt
+++ app/src/main/java/uk/co/suskins/commutestatus/service/CommuteStatusService.kt
@@ -1,11 +1,28 @@
 package uk.co.suskins.commutestatus.service
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PUT
 import uk.co.suskins.commutestatus.data.CommuteStatus
+import uk.co.suskins.commutestatus.data.Station
+import uk.co.suskins.commutestatus.data.User
+import uk.co.suskins.commutestatus.data.UserRequest
 
 interface CommuteStatusService {
     @GET("secure/commutestatus")
     fun getCommuteStatus(@Header("Authorization") accessToken: String?): Call<CommuteStatus>
+
+    @GET("public/stations")
+    fun getStations(): Call<List<Station>>
+
+    @GET("secure/user")
+    fun getUser(@Header("Authorization") accessToken: String?): Call<User>
+
+    @PUT("secure/user")
+    fun putUser(
+        @Header("Authorization") accessToken: String?,
+        @Body userRequest: UserRequest
+    ): Call<User>
 }
